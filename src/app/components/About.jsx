79ff8558648f9237b06c3735137065d29691191d
MVCorreas <correasmv@gmail.com>
2024-08-21 17:04:27 -0300

    Added About section

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import useEaseIn from '../hooks/useEaseIn';
+import { OutlinedButton } from "../utility/Buttons";
 
 export const AboutSection = () => {
   const router = useRouter();
@@ -13,36 +14,46 @@ export const AboutSection = () => {
   return (
     <section
     ref={(el) => (sectionsRef.current[0] = el)}
-      className="opacity-0 translate-y-10 animate-slideUp" 
+      className="opacity-0 translate-y-10 animate-slideUp bg-violet-200 h-90vh text-white " 
       id="about"
     >
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-8">
         <div className="flex flex-col justify-center">
-          <h2 className="text-4xl font-light text-yellow-400 mb-4">About Me</h2>
-          <p className="text-lg leading-relaxed">
-            Hi there! And welcome to my little abode. I am a Full Stack
-            developer from Mendoza, Argentina. Exactly! One of the Great Wine
-            Capitals. Not that I love wine, which I don’t; but it is a
-            historical icon of my land, and I am a fervent lover of tradition
-            and cultural inheritance. And so, let me tell you more about me, and
-            the story about my path towards web development.
-          </p>
-          <button
-            className="mt-6 btn btn-primary"
+          <h2 className="text-6xl font-light text-violet-400 mb-4 font-extrabold mb-8" style={{ fontWeight: '500' }}>About Fapro</h2>
+          <div className="text-lg leading-relaxed mb-20 text-black w-[500px]">
+           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus corporis ratione quaerat iusto cupiditate illo, deleniti culpa nesciunt sit ducimus molestiae rem doloribus quia similique. Harum recusandae animi ipsam neque.
+          </div>
+          <OutlinedButton
+            name='Find Out More'
             onClick={handleFindOutMore}
           >
-            FIND OUT MORE
-          </button>
+            
+          </OutlinedButton>
         </div>
         <div className="relative w-full h-full">
+        <div className="diff aspect-[16/9]">
+        <div className="diff-item-1">
           <Image
-            src="/Background3.jpg"
+            src="/Window2.jpg"
             alt="aboutImage"
-            width={900}
-            height={900}
+            width={800}
+            height={800}
             priority
             className="object-cover rounded-full"
           />
+          </div>
+          <div className="diff-item-2">
+          <Image
+            src="/Window1.jpeg"
+            alt="aboutImage"
+            width={800}
+            height={800}
+            priority
+            className="object-cover rounded-full"
+          />
+           </div>
+  <div className="diff-resizer"></div>
+</div>
         </div>
       </div>
     </section>
--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { HamburgerIcon } from "../../../public/Icons";
 import Image from "next/image";
 import { MainButton, OutlinedButton } from "../utility/Buttons";
@@ -20,7 +20,6 @@ const navLinks = [
 
 export const NavBar = () => {
   const router = useRouter();
-  const pathname = usePathname();
 
   const logIn = () => {
     router.push("/signin");
@@ -32,12 +31,12 @@ export const NavBar = () => {
 
   return (
     <div className="navbar fixed top-0 left-0 right-0 z-50 bg-white bg-opacity-50 backdrop-blur-lg h-24">
-      {/* Always displayed elements */}
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <HamburgerIcon />
           </div>
+
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
@@ -51,76 +50,26 @@ export const NavBar = () => {
         </div>
         <Image
           src="/Logo.png"
-          alt='logo'
           width={100}
           height={100}
           style={{ borderRadius: "10px" }}
           priority
         />
       </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 justify-between space-x-6 text-black text-lg">
+          {navLinks.map((link) => (
+            <li key={link.path}>
+              <a href={link.path}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="navbar-end gap-4">
+        <MainButton onClick={logIn} name="Sign In" />
 
-      {/* Conditionally displayed elements */}
-      {pathname === "/" && (
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 justify-between space-x-6 text-black text-lg">
-            {navLinks.map((link) => (
-              <li key={link.path}>
-                <a href={link.path}>{link.title}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      {pathname === "/signin" || pathname === "/register" ? (
-        <div className="navbar-end gap-4">
-          {pathname === "/signin" ? (
-            <MainButton onClick={signUp} name="Sign Up" />
-          ) : (
-            <MainButton onClick={logIn} name="Sign In" />
-          )}
-        </div>
-      ) : (
-        <div className="navbar-end gap-4">
-          <MainButton onClick={logIn} name="Sign In" />
-          <OutlinedButton onClick={signUp} name="Sign Up" />
-        </div>
-      )}
-
-      {pathname !== "/" && pathname !== "/signin" && pathname !== "/register" && (
-        <div className="navbar-end flex-none gap-2">
-          <div className="form-control">
-            <input
-              type="text"
-              placeholder="Search"
-              className="input input-bordered w-24 md:w-auto"
-            />
-          </div>
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Avatar"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li><a>Settings</a></li>
-              <li><a>Logout</a></li>
-            </ul>
-          </div>
-        </div>
-      )}
+        <OutlinedButton onClick={signUp} name="Sign Up" />
+      </div>
     </div>
   );
 };
