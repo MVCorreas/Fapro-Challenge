2e91d0a36e290100b1e9b79ef9cd1c38f4c9d924
MVCorreas <correasmv@gmail.com>
2024-08-22 19:04:37 -0300

    Added TableContent with functionalities

--- src/app/components/TableContent.jsx
+++ src/app/components/TableContent.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import Image from "next/image";
-import { EyeIcon, BinIcon, DownloadIcon, PlusIcon } from "../../../public/Icons";
+import { EyeIcon, BinIcon, PlusIcon, EditIcon } from "../../../public/Icons";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
@@ -13,12 +13,11 @@ export const TableContent = () => {
   const [disabledEntities, setDisabledEntities] = useState([]);
   const [showDisabledEntities, setShowDisabledEntities] = useState(false);
   const [error, setError] = useState(null);
-  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [selectedEntities, setSelectedEntities] = useState([]);
   const router = useRouter();
 
   const fetchEntities = async () => {
     const token = localStorage.getItem('token');
-
     if (!token) {
       setError('No token found');
       return;
@@ -34,7 +33,7 @@ export const TableContent = () => {
 
       const enabledEntities = response.data.data.filter(entity => entity.is_enabled);
       const disabledEntitiesList = response.data.data.filter(entity => !entity.is_enabled);
-      
+
       setEntities(enabledEntities);
       setDisabledEntities(disabledEntitiesList);
     } catch (error) {
@@ -55,8 +54,8 @@ export const TableContent = () => {
     }
 
     try {
-      await axios.patch(`${apiUrl}/api_entities/entities/${id}/`, 
-        { is_enabled: newStatus }, 
+      await axios.patch(`${apiUrl}/api_entities/entities/${id}/`,
+        { is_enabled: newStatus },
         {
           headers: {
             'Authorization': `Bearer ${token}`,
@@ -65,7 +64,6 @@ export const TableContent = () => {
         }
       );
 
-      // Re-fetch entities after updating the status
       fetchEntities();
     } catch (error) {
       console.error('Error updating entity status:', error);
@@ -73,7 +71,7 @@ export const TableContent = () => {
     }
   };
 
-  const removeEntity = async (id) => {
+  const handleRemoveEntity = async (id) => {
     if (window.confirm("Do you want to remove?")) {
       const token = localStorage.getItem('token');
       if (!token) {
@@ -91,12 +89,12 @@ export const TableContent = () => {
 
         if (response.status === 204) {
           console.log("Entity deleted successfully");
-          setEntities((prevEntities) => prevEntities.filter((entity) => entity.id !== id));
+          setEntities(prevEntities => prevEntities.filter(entity => entity.id !== id));
+          setDisabledEntities(prevDisabledEntities => prevDisabledEntities.filter(entity => entity.id !== id));
         } else {
           console.error(`Failed to delete entity, status code: ${response.status}`);
         }
 
-        // Re-fetch entities after deletion
         fetchEntities();
       } catch (error) {
         console.error("Failed to delete entity:", error.message);
@@ -106,37 +104,106 @@ export const TableContent = () => {
 
   const handleCreateEntity = () => router.push('/create-entity');
   const handleEditEntity = (entityId) => router.push(`/edit-entity/${entityId}`);
-  const logOut = () => router.push('/');
   const toggleDisabledEntities = () => setShowDisabledEntities(prev => !prev);
-
-
 
   const handleSelectAll = (e) => {
     if (e.target.checked) {
-      const allProductIds = tableData.map((product) => product.id);
-      setSelectedProducts(allProductIds);
+      const allEntitiesIds = [...entities.map(entity => entity.id), ...disabledEntities.map(entity => entity.id)];
+      setSelectedEntities(allEntitiesIds);
     } else {
-      setSelectedProducts([]);
+      setSelectedEntities([]);
     }
   };
 
-  const handleSelectProduct = (id) => {
-    if (selectedProducts.includes(id)) {
-      setSelectedProducts(selectedProducts.filter((productId) => productId !== id));
-    } else {
-      setSelectedProducts([...selectedProducts, id]);
-    }
+  const handleSelectEntity = (id) => {
+    setSelectedEntities(prev =>
+      prev.includes(id) ? prev.filter(entityId => entityId !== id) : [...prev, id]
+    );
   };
+
+  const renderTableRows = (entitiesList, showEditButton = true) => (
+    entitiesList.length > 0 ? (
+      entitiesList.map((entity) => (
+        <tr key={entity.id}>
+          <th>
+            <label>
+              <input
+                type="checkbox"
+                className="checkbox"
+                checked={selectedEntities.includes(entity.id)}
+                onChange={() => handleSelectEntity(entity.id)}
+              />
+            </label>
+          </th>
+          <td>
+            <div className="flex items-center gap-3">
+              <div className="avatar">
+                <div className="mask mask-squircle h-8 w-8">
+                  <Image
+                    src={entity.image ? entity.image : '/Rocket.png'}
+                    alt={entity.business_name}
+                    width={50}
+                    height={50}
+                  />
+                </div>
+              </div>
+              <div className="font-bold text-xs">{entity.business_name}</div>
+            </div>
+          </td>
+          <td>{entity.credential}</td>
+          <td className="text-xs">{new Date(entity.created_at).getFullYear()}</td>
+          <td>
+            <p>{entity.is_enabled ? 'Active' : 'Inactive'}</p>
+          </td>
+          <td>
+            <div className="flex gap-2">
+              <button onClick={() => handleEditEntity(entity.id)}>
+              <EditIcon  />
+              </button>
+         
+            {entity.is_enabled ? (
+               <button onClick={() => handleRemoveEntity(entity.id)}>
+               <BinIcon  />
+               </button>
+            ) : (
+              null
+            )}
+              
+            </div>
+          </td>
+        </tr>
+      ))
+    ) : (
+      <tr>
+        <td colSpan="6" className="text-center text-xs p-4">
+          No items found.
+        </td>
+      </tr>
+    )
+  );
 
   return (
     <div className="overflow-x-auto bg-white rounded-sm shadow m-2">
       <div className="flex flex-row items-center justify-between space-x-1 p-2">
         <div className="stat-desc font-semibold text-black">My Companies</div>
-        <button className="btn my-1 text-xs btn-sm" onClick={handleCreateEntity}>
-          <PlusIcon />
-         Add
-        </button>
+        <div className="justify-end gap-1">
+          <button
+            className="btn my-1 text-xs btn-sm bg-purple-700 hover:bg-violet-400 text-white"
+            onClick={handleCreateEntity}
+          >
+            <PlusIcon />
+            Add
+          </button>
+          <button
+            className="btn my-1 text-xs btn-sm bg-purple-700 hover:bg-violet-400 text-white"
+            onClick={toggleDisabledEntities}
+          >
+            <EyeIcon />
+            {showDisabledEntities ? 'Hide' : 'Show'} Inactive
+          </button>
+        </div>
       </div>
+
       <div className="max-h-72 overflow-y-auto">
         <table className="table min-w-full">
           <thead>
@@ -147,7 +214,7 @@ export const TableContent = () => {
                     type="checkbox"
                     className="checkbox"
                     onChange={handleSelectAll}
-                    checked={selectedProducts.length === entities.length}
+                    checked={selectedEntities.length === [...entities, ...disabledEntities].length}
                   />
                 </label>
               </th>
@@ -159,59 +226,11 @@ export const TableContent = () => {
             </tr>
           </thead>
           <tbody>
-            {entities.length > 0 ? (
-              entities.map((entity) => (
-                <tr key={entity.id}>
-                  <th>
-                    <label>
-                      <input
-                        type="checkbox"
-                        className="checkbox"
-                        checked={selectedProducts.includes(entity.id)}
-                        onChange={() => handleSelectProduct(entity.id)}
-                      />
-                    </label>
-                  </th>
-                  <td>
-                    <div className="flex items-center gap-3">
-                      <div className="avatar">
-                        <div className="mask mask-squircle h-6 w-6">
-                          <Image
-                            src={entity.image ? entity.image : '/ProfileAvatar.jpeg'}
-                            alt={entity.business_name}
-                            width={30}
-                            height={30}
-                          />
-                        </div>
-                      </div>
-                      <div className="font-bold text-xs">{entity.business_name}</div>
-                    </div>
-                  </td>
-                  <td>{entity.credential}</td>
-                  <td className="text-xs">{new Date(entity.created_at).getFullYear()}</td>
-                  <td>
-                    <p>{entity.is_enabled ? 'Active' : 'Inactive'}</p>
-                  </td>
-                 
-                    <td>
-                      <div className="flex gap-2">
-                        <EyeIcon />
-                        <BinIcon />
-                      </div>
-                    </td>
-                
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan="6" className="text-center text-xs p-4">
-                  No companies found.
-                </td>
-              </tr>
-            )}
+            {renderTableRows(entities)}
+            {showDisabledEntities && renderTableRows(disabledEntities, false)}
           </tbody>
         </table>
       </div>
     </div>
   );
-}  
+}
