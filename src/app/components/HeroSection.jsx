7067ef7fcacc06b05706e35a1723a9505365e32f
MVCorreas <correasmv@gmail.com>
2024-08-25 08:05:14 -0300

    Removed and replaced images

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -12,9 +12,14 @@ export const HeroSection = () => {
   };
 
   return (
-    <section className='bg-slate-900 text-white min-h-screen overflow-y-clip flex justify-center items-center space-x-12 pt-10 mb-8' id="hero">
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-violet-700 via-amber-100 to-transparent rounded-full h-[500px] w-[500px] z-0 blur-xl opacity-50 absolute top-1/8 -left-1 ml-[50px] transform -translate-x-1/8 -translate-1/2"></div>
-
+    <section className='text-white min-h-screen overflow-y-clip flex justify-center items-center space-x-12 pt-10 mb-8' id="hero">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src="/Assets/BackgroundVideo.mp4" // Replace with your video file
+      ></video>
       <div className="w-[40%] relative flex flex-col items-center z-10 gap-2">
         <p className='text-white mb-4 text-5xl sm:text-5xl lg:text-7xl lg:leading-normal  animate-slideUp delay-500 mt-12' style={{fontSize: '200'}}>
           A NEW GENERATION OF DATA {''}
@@ -37,52 +42,8 @@ export const HeroSection = () => {
        
       </div>
 
-      <div className="w-1/2 relative grid grid-cols-3 gap-4 mt-10 z-10">
-        <div className="col-span-1 ml-20 w-full mb-[200px]">
-          <Image
-            src='/HeroImage1.webp'
-            alt='hero1'
-            width={700}
-            height={700}
-            priority
-            className={`animate-slideUp delay-500`}
-            style={{ borderRadius: '10px' }}
-          />
-        </div>
-
-        <div className="col-span-2 relative -ml-10 mt-10 z-10">
-          <Image
-            src='/HeroImage6.png'
-            alt='hero1'
-            width={300}
-            height={300}
-            priority
-            className="animate-slideUp delay-500"
-          />
-        </div>
-
-        <div className="col-span-1 -mt-40 -ml-30">
-          <Image
-            src='/HeroImage4.png'
-            alt='hero1'
-            width={300}
-            height={300}
-            priority
-            className="animate-slideUp delay-500"
-          />
-        </div>
-
-        <div className="col-span-2 ml-20 w-full mb-[100px] -mt-10 z-10 h-auto">
-          <Image
-            src='/HeroImage7.jpg'
-            alt='hero1'
-            width={300}
-            height={300}
-            priority
-            className="rounded-md w-auto h-auto animate-slideUp delay-500"
-          />
-        </div>
-      </div>
+     
+      
     </section>
   );
 };
